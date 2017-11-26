a797f80ba351063a2570f5798af2013e37459e98
VLER <ycysvler@hotmail.com>
2017-11-26 08:42:19 +0800

    * 在agent数据下，增加了state数据

--- server/mongo/haschemas.js
+++ server/mongo/haschemas.js
@@ -1,38 +1,44 @@
 var mongodbconfig = require('../config/mongodb');
 var mongoose = require('mongoose');
 
-module.exports = class Schemas{
-    constructor(){
+module.exports = class Schemas {
+    constructor() {
         let uri = mongodbconfig.uri + 'ha';
         let conn = mongoose.createConnection(uri, mongodbconfig.options);
 
-        conn.then(function(db) {
+        conn.then(function (db) {
             console.log("ha mongodb connected!");
         });
 
         this.configSchema = new mongoose.Schema({
-            package: {type: String,index: {unique: true, dropDups: true}},   // 包名
+            package: {type: String, index: {unique: true, dropDups: true}},   // 包名
             version: {type: String},                // 版本
-            content:{type: String},                 // 配置内容
-            createtime:Date                         // 创建时间
+            content: {type: String},                 // 配置内容
+            createtime: Date                         // 创建时间
         });
-
-        this.Config =  conn.model('Config', this.configSchema,'config');
 
         this.serviceSchema = new mongoose.Schema({
-            package: {type: String,index: {unique: true, dropDups: true}},   // 包名
+            package: {type: String, index: {unique: true, dropDups: true}},   // 包名
             name: {type: String},                   // 服务名称
-            directory:{type: String}                // 配置内容
+            directory: {type: String}                // 配置内容
         });
 
-        this.Service =  conn.model('Service', this.configSchema,'service');
-
         this.agentSchema = new mongoose.Schema({
-            ip: {type: String,index: {unique: true, dropDups: true}},   // 包名
+            ip: {type: String, index: {unique: true, dropDups: true}},   // 包名
             agentid: {type: String}                   // agentid
         });
 
-        this.Agent =  conn.model('Agent', this.configSchema,'agent');
+        this.instanceSchema = new mongoose.Schema({
+            instanceid: {type: String},               // instanceid
+            agentid: {type: String},                  // agentid
+            package: {type: String}                  // package
+        });
+
+        this.Config = conn.model('Config', this.configSchema, 'config');
+        this.Service = conn.model('Service', this.serviceSchema, 'service');
+        this.Instance = conn.model('Instance', this.instanceSchema, 'instance');
+        this.Agent = conn.model('Agent', this.agentSchema, 'agent');
+
     }
 }
 
--- server/routes/mgr/agent.js
+++ server/routes/mgr/agent.js
@@ -9,11 +9,53 @@ module.exports = function (router) {
     // PaaS -> 重建索引 0：全量重建，1：增量重建
     router.get('/agents', (req, res, next) => {
         let Agent = getMongoPool('ha').Agent;
+        let Instance = getMongoPool('ha').Instance;
 
-        Agent.find(function (err, items) {
-            res.json(items);
+        Agent.find(function (err, ags) {
+            Instance.find(function(err, ins){
+                var result = [];
+                for(var akey in ags){
+                    var agent = JSON.parse( JSON.stringify( ags[akey]));
+
+                    agent.instances = [];
+
+                    for(var ikey in ins){
+                        var instance =JSON.parse( JSON.stringify(ins[ikey])) ;
+                        if(instance.agentid == agent.agentid){
+                            instance.time = null;
+                            instance.badge = -1;    // -1 未知， 0 正常， 1 超过5秒无心跳， 2 超过1分钟无心跳
+
+                            // 心跳数据
+                            if(global.heartbeats.hasOwnProperty(instance.instanceid)){
+                                instance.time = global.heartbeats[instance.instanceid];
+                                let heart = moment(instance.time);
+                                let now = moment();
+                                let seconds = now.diff(heart, 'seconds');
+
+                                instance.badge = 0;
+                                if(seconds>5){
+                                    instance.badge = 1;
+                                }
+                                if(seconds>60){
+                                    instance.badge = 2;
+                                }
+                            }
+
+                            instance.state = "";
+                            // 状态数据
+                            if(global.states.hasOwnProperty(instance.instanceid)){
+                                instance.state = global.states[instance.instanceid];
+                            }
+
+                            agent.instances.push(instance);
+
+                        }
+                    }
+                    result.push(agent);
+                }
+                res.json(result);
+            });
+
         });
     });
-
-
 }
